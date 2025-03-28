d40385b8a28d09250fb0d9195e2d972a793fd2c8
ong kit wei <ongkw@ongs-MacBook-Pro.local>
2025-03-28 18:07:23 +0800

    Scroll to calculated results upon clicking

--- src/components/CalculatorWorkflow.tsx
+++ src/components/CalculatorWorkflow.tsx
@@ -129,7 +129,7 @@ const CalculatorWorkflow = () => {
   }
 
   return (
-    <div className="flex flex-row gap-8 pt-3">
+    <div className="flex flex-row gap-8 pt-3" id="calculatedResults">
       <a
         className="relative inline-flex items-center justify-center px-14 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group mt-5 hover:cursor-pointer"
         onClick={handleClearInputFieldButton}
@@ -141,6 +141,7 @@ const CalculatorWorkflow = () => {
       <a
         className="relative inline-flex items-center justify-center px-14 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group mt-5 hover:cursor-pointer"
         onClick={handleCalculateButton}
+        href="#calculatedResults"
       >
         <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
         <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
